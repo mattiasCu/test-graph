a61224c70c6e10a9858e958c48ed659ed740da47
cu <cusui0210@gmail.com>
2024-06-18 19:04:35 +0200

    对数据集以及gearnet的一些测试，写了一个notebook，用于展示数据集以及gearnet的一些测试结果。

--- dataset_test.py
+++ dataset_test.py
@@ -1,12 +1,15 @@
 from torchdrug import core
-from torchdrug import datasets, transforms
+from torchdrug import datasets, transforms,layers
 from torchdrug.core import Registry as R
+from torchdrug.layers import geometry
 
 import torchdrug
 from torchdrug import data
 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+mpl.use('Agg')
+
 
 EnzymeCommission = R.search("datasets.EnzymeCommission")
 PV = R.search("transforms.ProteinView")
@@ -14,10 +17,40 @@ trans = PV(view = "residue")
 dataset = EnzymeCommission("~/scratch/protein-datasets/", test_cutoff=0.95, 
                            atom_feature="full", bond_feature="full", verbose=1, transform = trans)
 
-dataset.visualize()
-print(dataset.data.node_feature.shape)  # torch.Size([6, 69])
-print(dataset.data.edge_feature.shape)  # torch.Size([12, 19])
-plt.show()
+# 数据集第一个样本，前两个残基的原子
+protein = dataset[0]["graph"]
+is_first_two = (protein.residue_number == 1) | (protein.residue_number == 2)
+first_two = protein.residue_mask(is_first_two, compact=True)
+
+#first_two.visualize()
+#plt.savefig("fig/first_two.png")
+
+graph_construction_model = layers.GraphConstruction(node_layers=[geometry.AlphaCarbonNode()], 
+                                                    edge_layers=[geometry.SpatialEdge(radius=10.0, min_distance=5),
+                                                                 geometry.KNNEdge(k=10, min_distance=5),
+                                                                 geometry.SequentialEdge(max_distance=2)
+                                                                 ],
+                                                    #edge_feature="gearnet"
+                                                    )
+
+_protein = data.Protein.pack([protein])
+protein_ = graph_construction_model(_protein)
+print("Graph before: ", _protein)
+print("Graph after: ", protein_)
+
+"""
+is_first_two = (graph.residue_number == 1) | (graph.residue_number == 2)
+first_two = graph.residue_mask(is_first_two, compact=True)
+first_two.visualize()
+plt.savefig("fig/test_protein.png")
+"""
+
+
+degree = protein_.degree_in + protein_.degree_out
+print("Average degree: ", degree.mean())
+print("Maximum degree: ", degree.max())
+print("Minimum degree: ", degree.min())
+
 
 
 
--- test.py
+++ test.py
@@ -3,13 +3,7 @@ from torchdrug import data
 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-mpl.use('TkAgg')
+mpl.use('Agg')
 
 
-# 创建一个示例图
-mol = data.Molecule.from_smiles("C1=CC=CC=C1")
-mol.visualize()
-print(mol.node_feature.shape)  # torch.Size([6, 69])
-print(mol.edge_feature.shape)  # torch.Size([12, 19])
-plt.show()
 
