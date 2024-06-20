83a61e42285e4108069969e3458b9edc38dfe39f
cu <cusui0210@gmail.com>
2024-06-20 20:17:11 +0200

    更改

--- test.py
+++ test.py
@@ -15,7 +15,141 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 mpl.use('Agg')
 
+import os
+import sys
+import logging
+from itertools import islice
 
+import torch
+from torch import distributed as dist
+from torch import nn
+from torch.utils import data as torch_data
+
+from torchdrug import data, core, utils
+from torchdrug.core import Registry as R
+from torchdrug.utils import comm, pretty
+
+
+module = sys.modules[__name__]
+logger = logging.getLogger(__name__)
+
+
+class Engine(core.Configurable):
+
+    def __init__(self, task, train_set, valid_set, test_set, optimizer, scheduler=None, gpus=None, batch_size=1,
+                 gradient_interval=1, num_worker=0, logger="logging", log_interval=100):
+        self.rank = comm.get_rank()
+        self.world_size = comm.get_world_size()
+        self.gpus = gpus
+        self.batch_size = batch_size
+        self.gradient_interval = gradient_interval
+        self.num_worker = num_worker
+
+        if gpus is None:
+            self.device = torch.device("cpu")
+        else:
+            if len(gpus) != self.world_size:
+                error_msg = "World size is %d but found %d GPUs in the argument"
+                if self.world_size == 1:
+                    error_msg += ". Did you launch with `python -m torch.distributed.launch`?"
+                raise ValueError(error_msg % (self.world_size, len(gpus)))
+            self.device = torch.device(gpus[self.rank % len(gpus)])
+
+        if self.world_size > 1 and not dist.is_initialized():
+            if self.rank == 0:
+                module.logger.info("Initializing distributed process group")
+            backend = "gloo" if gpus is None else "nccl"
+            comm.init_process_group(backend, init_method="env://")
+
+        if hasattr(task, "preprocess"):
+            if self.rank == 0:
+                module.logger.warning("Preprocess training set")
+            # TODO: more elegant implementation
+            # handle dynamic parameters in optimizer
+            old_params = list(task.parameters())
+            result = task.preprocess(train_set, valid_set, test_set)
+            if result is not None:
+                train_set, valid_set, test_set = result
+            new_params = list(task.parameters())
+            if len(new_params) != len(old_params):
+                optimizer.add_param_group({"params": new_params[len(old_params):]})
+        if self.world_size > 1:
+            task = nn.SyncBatchNorm.convert_sync_batchnorm(task)
+            buffers_to_ignore = []
+            for name, buffer in task.named_buffers():
+                if not isinstance(buffer, torch.Tensor):
+                    buffers_to_ignore.append(name)
+            task._ddp_params_and_buffers_to_ignore = set(buffers_to_ignore)
+        if self.device.type == "cuda":
+            task = task.cuda(self.device)
+
+        self.model = task
+        self.train_set = train_set
+        self.valid_set = valid_set
+        self.test_set = test_set
+        self.optimizer = optimizer
+        self.scheduler = scheduler
+
+        if isinstance(logger, str):
+            if logger == "logging":
+                logger = core.LoggingLogger()
+            elif logger == "wandb":
+                logger = core.WandbLogger(project=task.__class__.__name__)
+            else:
+                raise ValueError("Unknown logger `%s`" % logger)
+        self.meter = core.Meter(log_interval=log_interval, silent=self.rank > 0, logger=logger)
+        self.meter.log_config(self.config_dict())
+        
+    def train(self, num_epoch=1, batch_per_epoch=None):
+
+        sampler = torch_data.DistributedSampler(self.train_set, self.world_size, self.rank)
+        dataloader = data.DataLoader(self.train_set, self.batch_size, sampler=sampler, num_workers=self.num_worker)
+        batch_per_epoch = batch_per_epoch or len(dataloader)
+        model = self.model
+        model.split = "train"
+        if self.world_size > 1:
+            if self.device.type == "cuda":
+                model = nn.parallel.DistributedDataParallel(model, device_ids=[self.device],
+                                                            find_unused_parameters=True)
+            else:
+                model = nn.parallel.DistributedDataParallel(model, find_unused_parameters=True)
+        model.train()
+
+        for epoch in self.meter(num_epoch):
+            sampler.set_epoch(epoch)
+
+            metrics = []
+            start_id = 0
+            # the last gradient update may contain less than gradient_interval batches
+            gradient_interval = min(batch_per_epoch - start_id, self.gradient_interval)
+
+            for batch_id, batch in enumerate(islice(dataloader, batch_per_epoch)):
+                if self.device.type == "cuda":
+                    batch = utils.cuda(batch, device=self.device)
+
+                loss, metric = model(batch)
+                if not loss.requires_grad:
+                    raise RuntimeError("Loss doesn't require grad. Did you define any loss in the task?")
+                loss = loss / gradient_interval
+                loss.backward()
+                metrics.append(metric)
+
+                if batch_id - start_id + 1 == gradient_interval:
+                    self.optimizer.step()
+                    self.optimizer.zero_grad()
+
+                    metric = utils.stack(metrics, dim=0)
+                    metric = utils.mean(metric, dim=0)
+                    if self.world_size > 1:
+                        metric = comm.reduce(metric, op="mean")
+                    self.meter.update(metric)
+
+                    metrics = []
+                    start_id = batch_id + 1
+                    gradient_interval = min(batch_per_epoch - start_id, self.gradient_interval)
+
+            if self.scheduler:
+                self.scheduler.step()
 
 truncate_transform = transforms.TruncateProtein(max_length=350, random=False)
 protein_view_transform = transforms.ProteinView(view="residue")
@@ -30,6 +164,7 @@ dataset = EnzymeCommission("~/scratch/protein-datasets/", test_cutoff=0.95,
                            atom_feature="full", bond_feature="full", verbose=1, transform = trans)
 
 train_set, valid_set, test_set = dataset.split()
+
 
 gearnet = models.GearNet(input_dim=21, hidden_dims=[512, 512, 512], num_relation=7,
                          batch_norm=True, concat_hidden=True, short_cut=True, readout="sum")
@@ -51,9 +186,21 @@ task = tasks.MultipleBinaryClassification(gearnet, graph_construction_model=grap
 
 
 optimizer = torch.optim.Adam(task.parameters(), lr=1e-4)
-solver = core.Engine(task, train_set, valid_set, test_set, optimizer,
+solver = Engine(task, train_set, valid_set, test_set, optimizer,
                      gpus=[0], batch_size=4)
+
+
+
+
+
+
 solver.train(num_epoch=2)
-solver.evaluate("valid")
+#solver.evaluate("valid")
+
+
+
+
+
+
 
 print(solver.evaluate("test"))
